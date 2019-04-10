b766dac038e128488c3d8988ccc2c242875ae454
lucasmbraz <lmonteirobraz@gmail.com>
2019-04-10 08:38:04 +0200

    Print message on MoviesApi#init to show that there are many instances of it

--- app/src/main/java/com/lucasmbraz/mcu/api/MoviesApi.kt
+++ app/src/main/java/com/lucasmbraz/mcu/api/MoviesApi.kt
@@ -1,5 +1,6 @@
 package com.lucasmbraz.mcu.api
 
+import android.util.Log
 import com.lucasmbraz.mcu.model.Movie
 import com.lucasmbraz.mcu.model.movies
 import io.reactivex.Observable
@@ -9,6 +10,11 @@ import java.util.concurrent.TimeUnit.SECONDS
 // Pretend this is an external class and you
 // cannot annotate the constructor with @Inject
 class MoviesApi {
+
+    init {
+        Log.d("MoviesApi", "Initialized")
+    }
+
     fun getMovies(): Single<List<Movie>> = Single.just(movies).delay(1, SECONDS)
 
     fun getMovie(movieId: String) = Single.fromObservable(
