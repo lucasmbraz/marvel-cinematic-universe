3356cd5584fb184ffde3e78a940a3c4acb954d44
lucasmbraz <lmonteirobraz@gmail.com>
2019-04-10 08:43:14 +0200

    Add @Singleton to both ApiModule and MoviesComponent to show that "it doesn't do anything"

--- app/src/main/java/com/lucasmbraz/mcu/movies/di/ApiModule.kt
+++ app/src/main/java/com/lucasmbraz/mcu/movies/di/ApiModule.kt
@@ -3,9 +3,10 @@ package com.lucasmbraz.mcu.movies.di
 import com.lucasmbraz.mcu.api.MoviesApi
 import dagger.Module
 import dagger.Provides
+import javax.inject.Singleton
 
 @Module
 class ApiModule {
 
-    @Provides fun api() = MoviesApi()
+    @Provides @Singleton fun api() = MoviesApi()
 }
--- app/src/main/java/com/lucasmbraz/mcu/movies/di/MoviesComponent.kt
+++ app/src/main/java/com/lucasmbraz/mcu/movies/di/MoviesComponent.kt
@@ -3,7 +3,9 @@ package com.lucasmbraz.mcu.movies.di
 import com.lucasmbraz.mcu.moviedetails.MovieDetailsFragment
 import com.lucasmbraz.mcu.movies.MoviesFragment
 import dagger.Component
+import javax.inject.Singleton
 
+@Singleton
 @Component(modules = [ApiModule::class])
 interface MoviesComponent {
     fun inject(fragment: MoviesFragment)
