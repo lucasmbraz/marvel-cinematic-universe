fe2163616b2e86cd3f3e89385a67e919519b7ed9
lucasmbraz <lmonteirobraz@gmail.com>
2019-04-10 06:21:16 +0200

    Annotate MoviesPresenter's constructor with @Inject

--- app/src/main/java/com/lucasmbraz/mcu/movies/MoviesPresenter.kt
+++ app/src/main/java/com/lucasmbraz/mcu/movies/MoviesPresenter.kt
@@ -1,8 +1,9 @@
 package com.lucasmbraz.mcu.movies
 
 import com.lucasmbraz.mcu.model.movies
+import javax.inject.Inject
 
-class MoviesPresenter {
+class MoviesPresenter @Inject constructor() {
     fun start(view: MoviesView) {
         view.showMovies(movies)
     }
