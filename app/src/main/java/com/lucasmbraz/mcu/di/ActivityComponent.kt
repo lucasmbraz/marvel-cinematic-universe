e3ad79a31b23182f2ca31388e8b4f6c72c5824e2
lucasmbraz <lmonteirobraz@gmail.com>
2019-05-16 08:58:40 +0200

    Attempt to use @Singleton in ActivityComponent to show that it conflicts with AppComponent

--- app/src/main/java/com/lucasmbraz/mcu/di/ActivityComponent.kt
+++ app/src/main/java/com/lucasmbraz/mcu/di/ActivityComponent.kt
@@ -3,7 +3,9 @@ package com.lucasmbraz.mcu.di
 import com.lucasmbraz.mcu.MainActivity
 import com.lucasmbraz.mcu.movies.MoviesFragment
 import dagger.Subcomponent
+import javax.inject.Singleton
 
+@Singleton
 @Subcomponent(modules = [PageChangerModule::class])
 interface ActivityComponent {
     fun inject(activity: MainActivity)
--- app/src/main/java/com/lucasmbraz/mcu/di/PageChangerModule.kt
+++ app/src/main/java/com/lucasmbraz/mcu/di/PageChangerModule.kt
@@ -4,10 +4,11 @@ import androidx.fragment.app.FragmentManager
 import com.lucasmbraz.mcu.PageChanger
 import dagger.Module
 import dagger.Provides
+import javax.inject.Singleton
 
 @Module
 class PageChangerModule(private val fragmentManager: FragmentManager) {
 
-    @Provides
+    @Provides @Singleton
     fun pageChanger() = PageChanger(fragmentManager)
 }
