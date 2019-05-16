8f2fbb304b51facdd3d7de248a3d0b7e0546f2e6
lucasmbraz <lmonteirobraz@gmail.com>
2019-05-16 08:56:51 +0200

    Print log message to show there are many instances of PageChanger being created

--- app/src/main/java/com/lucasmbraz/mcu/PageChanger.kt
+++ app/src/main/java/com/lucasmbraz/mcu/PageChanger.kt
@@ -1,11 +1,16 @@
 package com.lucasmbraz.mcu
 
+import android.util.Log
 import androidx.fragment.app.FragmentManager
 import com.lucasmbraz.mcu.model.Movie
 import com.lucasmbraz.mcu.moviedetails.MovieDetailsFragment
 import com.lucasmbraz.mcu.movies.MoviesFragment
 
 class PageChanger(private val fragmentManager: FragmentManager) {
+
+    init {
+        Log.d("PageChanger", "PageChanger init")
+    }
 
     fun showMoviesFragment() {
         fragmentManager.beginTransaction()
